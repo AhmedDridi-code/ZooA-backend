7f1c545a00ba5f868b20cc5db648501ad162859a
ZineddineBK-DEV <zineddineboubaker20@gmail.com>
2022-02-27 17:59:39 +0100

    fix all

--- app.js
+++ app.js
@@ -2,36 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 app.get('/', (req, res) => {
-    res.status(200).send({test:"Server is running ..... "});
-});
-app.get('/ons', (req, res) => {
-    res.status(200).send({test:"hello from the other side :D"});
-});
-
-
-app.get('/ons', (req, res) => {
-    res.status(200).send({test:"hello from the other side :D"});
-});
-
-app.get('/onsssssss', (req, res) => {
-    res.status(200).send({test:"hello from the other side :D"});
-});
-app.get('/onsssssss', (req, res) => {
-    res.status(200).send({test:"hello from the other side :D"});
-});
-app.get('/ons', (req, res) => {
-    res.status(200).send({test:"hello from the other side :D"});
-});
-
-
-app.get('/oussema', (req,res)=>{
-    res.status(200).send({mssage:"helloooo"})
+    res.status(200).send({test: "Server is running ..... "});
+    app.listen(8080);
+    console.log('listening on port ', port);
 })
-app.get('/zineddine', (req, res) => {
-    res.status(200).send({test:"Hello zineddine's branch"});
-});
-app.get('/zineddine2', (req, res) => {
-    res.status(200).send({test:"Hello zineddine's 2nd commt branch"});
-});
-app.listen(8080);
-console.log('listening on port ',port);    
+
