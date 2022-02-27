df0118eb4c7e0cd97b0452ae00dc61fbeaf3905d
Oussema Gharbi <oussemagharbi@Oussemas-MacBook-Air.local>
2022-02-27 17:29:02 +0100

    nothing push

--- app.js
+++ app.js
@@ -2,11 +2,24 @@ const express = require('express');
 const app = express();
 const port = 8080;
 app.get('/', (req, res) => {
-    res.status(200).send({test:"Hello World"});
+    res.status(200).send({test:"Server is running ..... "});
 });
+app.get('/ons', (req, res) => {
+    res.status(200).send({test:"hello from the other side :D"});
+});
+app.get('/onsssssss', (req, res) => {
+    res.status(200).send({test:"hello from the other side :D"});
+});
+
 
 app.get('/oussema', (req,res)=>{
     res.status(200).send({mssage:"helloooo"})
 })
+app.get('/zineddine', (req, res) => {
+    res.status(200).send({test:"Hello zineddine's branch"});
+});
+app.get('/zineddine2', (req, res) => {
+    res.status(200).send({test:"Hello zineddine's 2nd commt branch"});
+});
 app.listen(8080);
-console.log('listening on port 8080');
+console.log('listening on port ',port);
