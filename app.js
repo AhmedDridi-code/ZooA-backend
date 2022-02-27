96683f5800d9a480d39247e6f9402c3297da05ef
Oussema Gharbi <oussemagharbi@Oussemas-MacBook-Air.local>
2022-02-27 16:44:50 +0100

    test commit

--- app.js
+++ app.js
@@ -5,5 +5,8 @@ app.get('/', (req, res) => {
     res.status(200).send({test:"Hello World"});
 });
 
+app.get('/oussema', (req,res)=>{
+    res.status(200).send({mssage:"helloooo"})
+})
 app.listen(8080);
 console.log('listening on port 8080');
