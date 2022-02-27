4501670a86680abc4776f22dfff76fead23e06ab
onsyedes <ons.yedes@tek-up.de>
2022-02-27 15:47:12 -0800

    first commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 app.get('/', (req, res) => {
-    res.status(200).send({test:"Hello World"});
+    res.status(200).send({test:"Server is running .. "});
 });
 
 app.listen(8080);
