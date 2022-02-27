6aca9ea6b83fa8c0fd9a2e0723b39a7e63499f0c
Oussema Gharbi <oussemagharbi@Oussemas-MacBook-Air.local>
2022-02-27 17:29:51 +0100

    oussema main merge

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ const express = require('express');
 const app = express();
 const port = 8080;
 app.get('/', (req, res) => {
-    res.status(200).send({test:"Hello World"});
+    res.status(200).send({test:"Server is running ..... "});
 });
 
 app.get('/oussema', (req,res)=>{
     res.status(200).send({mssage:"helloooo"})
 })
+app.get('/ons', (req, res) => {
+    res.status(200).send({test:"hello from the other side :D"});
+});
+
+
 app.get('/zineddine', (req, res) => {
     res.status(200).send({test:"Hello zineddine's branch"});
+});
+app.get('/zineddine2', (req, res) => {
+    res.status(200).send({test:"Hello zineddine's 2nd commt branch"});
 });
 app.listen(8080);
 console.log('listening on port ',port);    
