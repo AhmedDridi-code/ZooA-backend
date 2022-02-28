5a5ab58229012c6f09e97176629657e33fedef8d
onsyedes <ons.yedes@tek-up.de>
2022-02-27 16:27:26 -0800

    second commit

--- app.js
+++ app.js
@@ -2,17 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8080;
 app.get('/', (req, res) => {
-    res.status(200).send({test:"Server is running .. "});
+    res.status(200).send({test:"Server is running ..... "});
+});
+app.get('/ons', (req, res) => {
+    res.status(200).send({test:"hello from the other side :D"});
 });
 
 
-app.get('/ahmed', (req, res) => {
-    res.status(200).send({test:"Hello from Ahmed Branch"});
-});
-
-
-app.get('/zineddine', (req, res) => {
-    res.status(200).send({test:"Hello zineddine's branch"});
-});
 app.listen(8080);
 console.log('listening on port ',port);
