00f6fba44541aebce9250d72edcafed45b36bb65
ZineddineBK-DEV <zineddineboubaker20@gmail.com>
2022-03-15 14:28:01 +0100

    zineddine changes

--- src/models/post.js
+++ src/models/post.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const postSchema = mongoose.Schema({
     description:{type:String, required:true},
     image:string,
-    date:{type:Date, required:true},
+    date:{type:Date, required:true, default:new Date()},
     user : {
             type : mongoose.Schema.Types.ObjectId,
             ref:"User"
--- src/routes/post.js
+++ src/routes/post.js
@@ -1,19 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('./post');
 
 //get all posts
-router.get('/',(req, res)=>{
-
+router.get('/posts',(req, res)=>{
+const posts = Post.find({}, (err,data)=>{
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log(data);
+    }
+    });
+    res.status(200).json(posts);
 })
 
 //add a post
-router.post('/',(req, res)=>{
-
+router.post('/post',(req, res)=>{
+    const post = new Post({
+        desc: req.body.description,
+        img: req.body.image,
+      })
+      post.save(function (err, post) {
+        if (err) { return next(err) }
+        res.json(201, post)
+      })
+      res.status(200).json(post);
 })
 
 //delete a post
-router.delete('/:id',(req, res)=>{
-
+router.delete('/post/:id',(req, res)=>{
+    const post = Post.findById({_id :req.body.})
 })
 
 //update a post
