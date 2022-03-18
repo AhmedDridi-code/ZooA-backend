11374d46420c73bbac5c1496f9fe7f65e6068a14
onsyedes <ons.yedes@tek-up.de>
2022-03-18 00:34:35 -0700

    --

--- src/routes/upgradeRequest.js
+++ src/routes/upgradeRequest.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {upload} =require('../middlewares/fileStorageEngine')
+const {upload} =require('../outils/fileStorageEngine')
 
 
 const upgradeRequestController = require('../controllers/upgradeRequestController')
